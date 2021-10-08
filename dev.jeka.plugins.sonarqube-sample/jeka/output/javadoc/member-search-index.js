62a29158911aa0377c0dbbad85710191a9d377fc
M999JAD <Jerome.Angibaud@m-team.be>
2021-10-08 10:04:42 +0200

    Release:1.0.0.RC1 #1

--- dev.jeka.plugins.sonarqube-sample/jeka/output/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"your.basepackage","c":"Application","l":"Application()","url":"%3Cinit%3E()"},{"p":"your.basepackage","c":"Application","l":"main(String[])","url":"main(java.lang.String[])"}]
