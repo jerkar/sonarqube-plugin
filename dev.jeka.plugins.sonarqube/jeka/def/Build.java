38bdf052032791f4340b3a3cb10e032703c10d20
M999JAD <Jerome.Angibaud@m-team.be>
2021-10-08 10:41:04 +0200

    Release:1.0.0.RC1 #1

--- dev.jeka.plugins.sonarqube/jeka/def/Build.java
+++ dev.jeka.plugins.sonarqube/jeka/def/Build.java
@@ -1,65 +1,65 @@
+import dev.jeka.core.api.depmanagement.JkRepoSet;
+import dev.jeka.core.api.depmanagement.JkVersion;
 import dev.jeka.core.api.java.JkJavaVersion;
 import dev.jeka.core.api.system.JkLocator;
-import dev.jeka.core.api.tooling.JkGitProcess;
 import dev.jeka.core.tool.JkClass;
+import dev.jeka.core.tool.JkEnv;
 import dev.jeka.core.tool.JkPlugin;
-import dev.jeka.core.tool.builtins.git.JkPluginGit;
 import dev.jeka.core.tool.builtins.java.JkPluginJava;
-
-import java.util.Optional;
+import dev.jeka.core.tool.builtins.release.JkPluginVersionFromGit;
+import dev.jeka.core.tool.builtins.repos.JkPluginGpg;
 
 class Build extends JkClass {
 
-    final JkPluginJava javaPlugin = getPlugin(JkPluginJava.class);
+    private final JkPluginJava java = getPlugin(JkPluginJava.class);
 
-    final JkPluginGit gitPlugin = getPlugin(JkPluginGit.class);
+    @JkEnv("OSSRH_USER")
+    public String ossrhUser;
+
+    @JkEnv("OSSRH_PWD")
+    public String ossrhPwd;
+
+    final JkPluginGpg gpg = getPlugin(JkPluginGpg.class);
+
+    final JkPluginVersionFromGit versionFromGit = getPlugin(JkPluginVersionFromGit.class);
 
     @Override
     protected void setup() {
-        javaPlugin.getProject().simpleFacade()
-            .setJavaVersion(JkJavaVersion.V8)
-            .setSimpleLayout()
-            .mixResourcesAndSources()
-            .setCompileDependencies(deps -> deps
-                .andFiles(JkLocator.getJekaJarPath())
-            );
-        JkPlugin.setJekaPluginCompatibilityRange(javaPlugin.getProject().getConstruction().getManifest(),
-                "0.9.15.M1",
+        JkPlugin.setJekaPluginCompatibilityRange(java.getProject().getConstruction().getManifest(),
+                "0.9.15.M2",
                 "https://raw.githubusercontent.com/jerkar/sonarqube-plugin/master/breaking_versions.txt");
+        java.getProject().simpleFacade()
+                .setJavaVersion(JkJavaVersion.V8)
+                .mixResourcesAndSources()
+                .setSimpleLayout()
+                .setCompileDependencies(deps -> deps
+                        .andFiles(JkLocator.getJekaJarPath())
+                );
 
-
-                // This section is necessary to publish on a public repository
-        javaPlugin.getProject().getPublication()
-                .getMaven()
-                    .setModuleId("dev.jeka:sonarqube-plugin")
-                    .setVersion(this::version)
-                    .getPomMetadata()
+        java.getProject().getPublication().getMaven()
+                .setModuleId("dev.jeka:sonarqube-plugin")
+                .setRepos(JkRepoSet.ofOssrhSnapshotAndRelease(ossrhUser, ossrhPwd,
+                        gpg.get().getSigner("")))
+                .getPomMetadata()
+                    .getProjectInfo()
+                        .setName("Jeka plugin for Sonarqube")
+                        .setDescription("A Jeka plugin for Jacoco coverage tool")
+                        .setUrl("https://github.com/jerkar/sonarqube-plugin")
+                    .__
+                    .getScm()
+                        .setUrl("https://github.com/jerkar/sonarqube-plugin").__
                         .addApache2License()
-                        .getProjectInfo()
-                            .setName("Jeka plugin for SonarQube")
-                            .setDescription("A Jeka plugin for SonarQube")
-                            .setUrl("https://github.com/jerkar/sonarqube-plugin").__
-                        .getScm()
-                            .setUrl("https://github.com/jerkar/sonarqube-plugin").__.__.__
-                .getPostActions()
-                    .append(this::tagIfNeeded);
+                        .addGithubDeveloper("djeang", "djeangdev@yahoo.fr");
+
+        // Make javadoc only for releases
+        if (!JkVersion.of(java.getProject().getPublication().getVersion()).isSnapshot()) {
+            java.pack.javadoc = true;
+        }
     }
 
     public void cleanPack() {
-        clean(); javaPlugin.pack();
+        clean(); java.pack();
     }
 
-    private String version() {
-        String currentTagVersion = gitPlugin.getWrapper().getVersionFromTag();
-        currentTagVersion = currentTagVersion.equals("HEAD-SNAPSHOT") ? "master-SNAPSHOT" : currentTagVersion;
-        String releaseVersion = gitPlugin.getWrapper().extractSuffixFromLastCommitMessage("Release:");
-        return Optional.ofNullable(releaseVersion).orElse(currentTagVersion);
-    }
-
-    private void tagIfNeeded() {
-        JkGitProcess git = gitPlugin.getWrapper();
-        Optional.ofNullable(git.extractSuffixFromLastCommitMessage("Release:"))
-                .ifPresent(version -> git.tagAndPush(version));
-    }
 
 }
